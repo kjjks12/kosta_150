7798f2078d1c6af51d2323cf7d12c0ce73cd2236
jgs <kjjks12@naver.com>
2017-02-13 09:08:14 +0900

    2월 6일 1.패키지 2.abstract Final 학습 3.interface학습

---
+++ ex0206/PackageTest.java
@@ -0,0 +1,13 @@
+package jeong.gwang.su;
+
+import jeong.jin.su.Test;
+
+class PackageTest 
+{
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Hello World!");
+		Test t= new Test();
+	}
+}
---
+++ ex0206/Test.java
@@ -0,0 +1,9 @@
+package jeong.jin.su;
+
+public class Test
+{
+	public void aa(){
+
+		System.out.println("test 객체의 aa입니다");
+	}
+}
