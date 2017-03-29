79a59bb82885e617e38745a7424876777f43c418
jgs <kjjks12@naver.com>
2017-03-29 11:04:26 +0900

    0322 JAVASCRIPT

---
+++ ex0322/외부js문서.js
@@ -0,0 +1,14 @@
+//주석
+alert("잘 나오네??");
+document.write("<h3>이젠 밥먹으러 갑시다 !!</h3>");
+
+
+//함수작성
+function test(){
+	alert("test()함수 입니다 .");
+	document.bgColor="blue";
+}
+
+function test2(){
+	document.bgColor="white";
+}
